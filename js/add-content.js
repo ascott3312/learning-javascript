6867e4727edbb67e632955c8fbd9d0d145c04a2f
YOUR FULL NAME <andrewscott@Andrews-MacBook-Air.local>
2020-06-04 14:03:50 -0500

    add

--- js/add-content.js
+++ js/add-content.js
@@ -2,7 +2,7 @@ var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 
-if (hourNow) > 18) {
+if (hourNow > 18) {
     greeting = 'Good evening!';
 } else if (hourNow) > 12) {
     greeting = 'Good afternoon!';
