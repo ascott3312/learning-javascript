f2a2684f1398aaf21b12c35b7d2389e0eb4245c9
YOUR FULL NAME <andrewscott@Andrews-MacBook-Air.local>
2020-06-03 22:22:26 -0500

    Add

--- js/add-content.js
+++ js/add-content.js
@@ -5,11 +5,11 @@ var greeting;
 if (hourNow) > 18) {
     greeting = 'Good evening!';
 } else if (hourNow) > 12) {
-    greeting = 'Good afternoon';
+    greeting = 'Good afternoon!';
 } else if (hourNow) > 0) {
-    greeting = 'Good morning';
+    greeting = 'Good morning!';
 } else {
-    greeting = 'Welcome';
+    greeting = 'Welcome!';
 }
 
 document.write('<h3>' + greeting + '<h3>');
